ae4faf212122ca83954a3292eef6db1f490d8f3e
Where <liuzhaowei55@gmail.com>
2022-03-15 14:05:46 +0800

    dotdrop

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # dotfiles
+
+## 安装
+
+```bash
+# 安装 dotdrop
+pip3 install -r dotdrop/requirements.txt --user
+# 安装依赖
+brew install coreutils
+# 使用帮助
+./dotdrop.sh -h
+# 对比
+./dotdrop.sh compare --profile=home
+# 复制 dotfiles
+./dotdrop.sh install --profile=home
+
+```
--- dotfiles/.oh-my-zsh/custom/alias.zsh
+++ dotfiles/.oh-my-zsh/custom/alias.zsh
@@ -1,3 +1,7 @@
 # 对 macOS 下的软件进行签名，解决 “已损坏，无法打开。 您应该将它移到废纸篓。” 的问题
 alias sign="sudo xattr -r -d com.apple.quarantine "
+
+# https://github.com/deadc0de6/dotdrop#as-a-submodule
+alias dotdrop="$HOME/.dotfiles/dotdrop.sh --cfg=$HOME/.dotfiles/config.yaml"
+
 alias ls="ls -all"
