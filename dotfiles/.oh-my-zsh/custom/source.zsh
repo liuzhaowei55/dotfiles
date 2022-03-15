4c616a375d266bdfd9ea68b1a7ac70057d2102a6
where.liu <where.liu@huolala.cn>
2022-03-15 16:07:05 +0800

    feat: save

---
+++ dotfiles/.oh-my-zsh/custom/source.zsh
@@ -0,0 +1,4 @@
+# 为 terminal 添加语法高亮
+source $HOME/.dotfiles/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# 为 terminal 添加命令补全功能
+source $HOME/.dotfiles/zsh-autosuggestions/zsh-autosuggestions.zsh
