9362735f21dc00c220d2f375b824eeacd6aed803
Where <liuzhaowei55@gmail.com>
2022-05-06 21:35:08 +0800

    feat: java

--- dotfiles/.oh-my-zsh/custom/export.zsh
+++ dotfiles/.oh-my-zsh/custom/export.zsh
@@ -7,3 +7,8 @@ fi
 if [ -d $HOME/.composer/vendor/bin ]; then
   export PATH=$HOME/.composer/vendor/bin:$PATH
 fi
+
+# java 
+# sudo ln -sfn /usr/local/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk
+export PATH="/usr/local/opt/openjdk@11/bin:$PATH"
+export CPPFLAGS="-I/usr/local/opt/openjdk@11/include"
