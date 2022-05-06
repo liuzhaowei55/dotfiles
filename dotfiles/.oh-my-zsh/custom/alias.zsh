8b732d13e26b7d9461d62fcca1049d34a00f5ce9
Where <liuzhaowei55@gmail.com>
2022-05-06 21:39:22 +0800

    feat: java

--- dotfiles/.oh-my-zsh/custom/alias.zsh
+++ dotfiles/.oh-my-zsh/custom/alias.zsh
@@ -1,7 +1,4 @@
 # 对 macOS 下的软件进行签名，解决 “已损坏，无法打开。 您应该将它移到废纸篓。” 的问题
 alias sign="sudo xattr -r -d com.apple.quarantine "
 
-# https://github.com/deadc0de6/dotdrop#as-a-submodule
-alias dotdrop="$HOME/.dotfiles/dotdrop.sh --cfg=$HOME/.dotfiles/config.yaml"
-
-alias ls="ls -all -G"
+alias ls="ls -G"
