d126525bdf03772501b27aaaa01e143f35f989a1
where.liu <where.liu@huolala.cn>
2022-05-07 09:51:27 +0800

    feat: java

--- dotfiles/.oh-my-zsh/custom/export.zsh
+++ dotfiles/.oh-my-zsh/custom/export.zsh
@@ -8,7 +8,7 @@ if [ -d $HOME/.composer/vendor/bin ]; then
   export PATH=$HOME/.composer/vendor/bin:$PATH
 fi
 
-# java 
-# sudo ln -sfn /usr/local/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk
-export PATH="/usr/local/opt/openjdk@11/bin:$PATH"
-export CPPFLAGS="-I/usr/local/opt/openjdk@11/include"
+# java
+# sudo ln -sfn $HOMEBREW_PREFIX/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk
+export PATH="$HOMEBREW_PREFIX/opt/openjdk@11/bin:$PATH"
+export CPPFLAGS="-I$HOMEBREW_PREFIX/opt/openjdk@11/include"
