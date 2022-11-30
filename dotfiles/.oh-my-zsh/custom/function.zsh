7cbe0337c1d98356ac930b48ea00d0a5a6909f14
where.liu <where.liu@huolala.cn>
2022-11-30 12:20:38 +0800

    feat: .oh-my-zsh/custom/function

--- dotfiles/.oh-my-zsh/custom/function.zsh
+++ dotfiles/.oh-my-zsh/custom/function.zsh
@@ -1,5 +1,6 @@
 #!/usr/bin/evn zsh
 
+# 杀死指定端口对应的进程
 function kill_port() {
     lsof -ti :"$1" | xargs kill -9
 }
--- dotfiles/.oh-my-zsh/custom/source.zsh
+++ dotfiles/.oh-my-zsh/custom/source.zsh
@@ -2,5 +2,3 @@
 source $HOME/.dotfiles/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 # 为 terminal 添加命令补全功能
 source $HOME/.dotfiles/zsh-autosuggestions/zsh-autosuggestions.zsh
-# 添加一些常用命令
-source $HOME/bin/command_helper.sh
