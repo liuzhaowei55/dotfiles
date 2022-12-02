07e80ac3b270ab5b713a6d18b0ec9bd146ece74b
where.liu <where.liu@huolala.cn>
2022-12-02 10:41:21 +0800

    feat: nvm

--- dotfiles/.oh-my-zsh/custom/export.zsh
+++ dotfiles/.oh-my-zsh/custom/export.zsh
@@ -12,12 +12,14 @@ if [ -d $HOME/.composer/vendor/bin ]; then
   PATH=$HOME/.composer/vendor/bin:$PATH
 fi
 
-# java
-# sudo ln -sfn $HOMEBREW_PREFIX/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk
-
 # libressl
 export PATH="$HOMEBREW_PREFIX/opt/libressl/bin:$PATH"
 
 #GnuPG
 export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
 export GPG_TTY="$(tty)"
+
+# nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"                                       # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
