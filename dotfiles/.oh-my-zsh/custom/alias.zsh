046c1a8babab48444699acffd28060e8497652d4
where.liu <where.liu@huolala.cn>
2022-12-08 18:01:13 +0800

    feat: save

--- dotfiles/.oh-my-zsh/custom/alias.zsh
+++ dotfiles/.oh-my-zsh/custom/alias.zsh
@@ -1,8 +1,6 @@
 # 对 macOS 下的软件进行签名，解决 “已损坏，无法打开。 您应该将它移到废纸篓。” 的问题
 alias sign="sudo xattr -r -d com.apple.quarantine"
 
-alias ls="ls -G"
-
 # gpg-agent
 alias ga-start="gpg-agent --daemon"
 alias ga-stop="gpgconf kill gpg-agent"
