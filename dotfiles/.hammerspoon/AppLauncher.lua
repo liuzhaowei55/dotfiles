38e71ec64a0aa8e98490dcdd1909996c54dcd890
where.liu <where.liu@huolala.cn>
2023-01-07 16:16:23 +0800

    feat: hammerspoon

--- dotfiles/.hammerspoon/AppLauncher.lua
+++ dotfiles/.hammerspoon/AppLauncher.lua
@@ -3,7 +3,12 @@ hs.hotkey.bind({"ctrl", "alt", "shift"}, "A", function()
     hs.application.open("Arc")
 end)
 
--- 打开 Toolbox
+-- 打开 Toolbox JSON Editor
 hs.hotkey.bind({"ctrl", "alt", "shift"}, "J", function()
-    hs.urlevent.openURL("https://toolbox.fanfou.dev/code-formatter")
+    hs.urlevent.openURL("https://toolbox.fanfou.dev/json")
+end)
+
+-- 打开 Terminal
+hs.hotkey.bind({"ctrl", "alt", "shift"}, "T", function()
+    hs.application.open("Terminal")
 end)
