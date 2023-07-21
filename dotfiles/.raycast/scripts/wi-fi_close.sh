e66791b986650cc411dfb28136b592c2403dec05
liuzhaowei55 <liuzhaowei55@gmail.com>
2023-07-21 10:02:47 +0800

    feat: proxy state

--- dotfiles/.raycast/scripts/wi-fi_close.sh
+++ dotfiles/.raycast/scripts/wi-fi_close.sh
@@ -12,4 +12,11 @@
 # @raycast.description Wi-Fi 管理
 # @raycast.author Where.LIU
 
+# https://debugtalk.com/post/switch-macOS-web-proxy-in-elegant-way/
+# networksetup -listallnetworkservices
+
+networksetup -setwebproxystate USB\ 10/100/1000\ LAN off
+networksetup -setsecurewebproxystate USB\ 10/100/1000\ LAN off
+networksetup -setsocksfirewallproxystate USB\ 10/100/1000\ LAN off
+
 networksetup -setairportpower en0 off
--- dotfiles/.raycast/scripts/wi-fi_open.sh
+++ dotfiles/.raycast/scripts/wi-fi_open.sh
@@ -12,4 +12,8 @@
 # @raycast.description Wi-Fi 管理
 # @raycast.author Where.LIU
 
+networksetup -setwebproxystate USB\ 10/100/1000\ LAN off
+networksetup -setsecurewebproxystate USB\ 10/100/1000\ LAN off
+networksetup -setsocksfirewallproxystate USB\ 10/100/1000\ LAN off
+
 networksetup -setairportpower en0 on
