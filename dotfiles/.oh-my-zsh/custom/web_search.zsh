3ccefca8e96a4efc202cd8f4acbdb79a01e7b7ce
liuzhaowei55 <liuzhaowei55@gmail.com>
2024-02-26 14:53:40 +0800

    feat: web_search

--- dotfiles/.oh-my-zsh/custom/web_search.zsh
+++ dotfiles/.oh-my-zsh/custom/web_search.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 ZSH_WEB_SEARCH_ENGINES=(
-    npmjs "https://www.npmjs.com/search?q="
+    mvn "https://mvnrepository.com/search?q="
 )
 
-alias npmjs="web_search npmjs"
+alias "searchmvn"="web_search mvn"
