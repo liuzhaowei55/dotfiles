0ab1aa0b4993abd8fe4c1b5d7ac52e28fcdc1009
liuzhaowei55 <liuzhaowei55@gmail.com>
2024-04-26 11:32:46 +0800

    feat: ddd

--- dotfiles/.oh-my-zsh/custom/export.zsh
+++ dotfiles/.oh-my-zsh/custom/export.zsh
@@ -16,8 +16,6 @@ if [ -d $HOME/go/bin ]; then
   PATH=$HOME/go/bin:$PATH
 fi
 
-export PATH="$HOMEBREW_PREFIX/opt/node@20/bin:$PATH"
-
 # libressl
 export PATH="$HOMEBREW_PREFIX/opt/libressl/bin:$PATH"
 
@@ -27,8 +25,3 @@ export JAVA_HOME=$(/usr/libexec/java_home -v 17)
 #GnuPG
 export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
 export GPG_TTY="$(tty)"
-
-# nvm
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"                                       # This loads nvm
-# [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
