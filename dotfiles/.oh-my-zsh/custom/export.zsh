91822f5725f9e26eed4e65c1e040dfe62c37548a
liuzhaowei55 <liuzhaowei55@gmail.com>
2024-08-30 10:42:25 +0800

    feat: export

--- dotfiles/.oh-my-zsh/custom/export.zsh
+++ dotfiles/.oh-my-zsh/custom/export.zsh
@@ -1,23 +1,25 @@
 #!/usr/bin/env zsh
 
-if [ -d $HOME/bin ]; then
-  PATH=$HOME/bin:$PATH
+if [ -d "$HOME/bin" ]; then
+  export PATH="$HOME/bin:$PATH"
 fi
 
 if [ -d "$HOME/.local/bin" ]; then
-  PATH="$HOME/.local/bin:$PATH"
+  export PATH="$HOME/.local/bin:$PATH"
 fi
 
-if [ -d $HOME/.composer/vendor/bin ]; then
-  PATH=$HOME/.composer/vendor/bin:$PATH
+if [ -d "$HOME/.composer/vendor/bin" ]; then
+  export PATH="$HOME/.composer/vendor/bin:$PATH"
 fi
 
-if [ -d $HOME/go/bin ]; then
-  PATH=$HOME/go/bin:$PATH
+if [ -d "$HOME/go/bin" ]; then
+  export PATH="$HOME/go/bin:$PATH"
 fi
 
 # libressl
-export PATH="$HOMEBREW_PREFIX/opt/libressl/bin:$PATH"
+if [ -d "$HOMEBREW_PREFIX/opt/libressl/bin" ]; then
+  export PATH="$HOMEBREW_PREFIX/opt/libressl/bin:$PATH"
+fi
 
 # java default version
 export JAVA_HOME=$(/usr/libexec/java_home -v 21)
